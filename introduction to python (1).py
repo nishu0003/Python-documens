b3ca0ab2b6c17266b66397d34c4bec299b842b7e
nishu0003 <99498373+nishu0003@users.noreply.github.com>
2022-02-21 11:29:59 +0530

    introdction of python

---
+++ introduction to python (1).py
@@ -0,0 +1,274 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#   # print_statement
+
+# In[3]:
+
+
+#syntax
+print("hello chapri log")
+
+
+# In[ ]:
+
+
+#  ywqeyuy
+#     ewuyiew
+#     ewu
+#     ewiqou
+
+
+# In[9]:
+
+
+# variable
+
+
+# In[16]:
+
+
+b="i am nishu" #string ---datatype
+print(b)
+
+
+# In[15]:
+
+
+a=5
+print(a)
+
+
+# In[18]:
+
+
+print(b)
+
+
+# In[19]:
+
+
+#type() ---it tells thr datatyoe of any variable
+
+
+# In[21]:
+
+
+print(type(a))
+print(type(b))
+
+
+# In[23]:
+
+
+d=["nishant",5,764,"ramu"]
+e=("chauhan",763,93,"jhamura")
+print(type(d))
+print(type(e))
+
+
+# In[25]:
+
+
+f={"name":"dewan"}
+print(f)
+
+
+# In[27]:
+
+
+print(type(f))
+
+
+# In[29]:
+
+
+#DATATYPE WITH Variables
+
+5=5
+print(5)         #you can't take integer variable
+
+
+# In[35]:
+
+
+Bjn =8
+print(Bjn)
+
+
+# In[39]:
+
+
+my name = "nishu"
+print(my name)     #you can't gave the space between variable beside that you can use "snake case" so that you don't get error
+
+
+# In[40]:
+
+
+my_name= "nishu chauhan"
+print(my_name)
+
+
+# In[42]:
+
+
+2ramu = 56
+print(2ramu)   #you can't use number in starting while declaration but you can use that after character
+
+
+# In[44]:
+
+
+ramu2 = 56
+print(ramu2)
+
+
+# In[46]:
+
+
+@name = "nishu"
+print(name)   #you can't use any spacial character in variable in starting as well as in last
+
+
+# # data_types
+
+# # Number---1)DATATYPE
+
+# # int,float,complex
+
+# In[54]:
+
+
+a="5"
+print(a)
+print(type(a))
+
+
+# In[56]:
+
+
+a=2.6
+print(type(a))
+
+
+# In[58]:
+
+
+b=9+7i
+print(b)
+
+
+# c=8+2j
+# print(c)
+# print(type(c))   #you can use only j as a imaginary term otherwise if you take any other character it shows error
+
+# # String
+
+# In[67]:
+
+
+b='namuna hai tu'
+print(b)
+print(type(b))
+
+
+# In[64]:
+
+
+sum = "67.6"
+print(sum)
+print(type(sum))
+
+
+# In[66]:
+
+
+b='''yyewquee
+euhuewr
+rwekjhkr
+reooi'''
+print(b)                 
+
+
+# # tuple
+
+# In[69]:
+
+
+a=("nishu",2.6,7,"ramu")
+print(a)
+print(type(a))
+
+
+# In[71]:
+
+
+b=["nishant",7,7.8]
+print(b)
+print(type(b))
+
+
+# # DICTIONARY
+
+# In[74]:
+
+
+A = {"name" : "nishant chauhan" , "age" : 22}
+print(A)
+
+
+# # TYPE CASTING
+
+# In[79]:
+
+
+b=str(5)
+print(b)
+
+
+# In[82]:
+
+
+c=str(7.6)
+print(c)
+print(type(c))
+
+
+# In[85]:
+
+
+b=str(65)
+print(b)
+print(type(b))
+
+
+# In[87]:
+
+
+c=float(89)
+print(c)
+print(type(c))                #you cant change the string into int or float
+
+
+# In[89]:
+
+
+a=int("nishu")
+print(a)
+print(type(a))  
+
+
+# In[91]:
+
+
+a=int("234")
+print(a)
+print(type(a))            
+
+
+# In[ ]:
+
+
+
+
